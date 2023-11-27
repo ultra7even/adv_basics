b1ec165c65608b2b61a267bbbd1873a2554077e3
Igor.Vict <mol.ultra@gmail.com>
2023-11-27 12:10:58 +0300

    Initial commit Ver.0.0.1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
