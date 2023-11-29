7002fcbbbee7428fc6c11505197ad6b8b08b293c
Igor.Vict <mol.ultra@gmail.com>
2023-11-29 17:05:13 +0300

    Added folders and files Ver.0.1.0

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-  
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
@@ -12,6 +14,39 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: currentQuestion.answers[0], 
+            onTap: () {},
+            ),
+           AnswerButton(
+            answerText: currentQuestion.answers[1], 
+            onTap: () {},
+            ),
+           AnswerButton(
+            answerText: currentQuestion.answers[2], 
+            onTap: () {},
+            ),
+           AnswerButton(
+            answerText: currentQuestion.answers[3], 
+            onTap: () {},
+            ),    
+        ],
+      ),
+    );
   }
 }
